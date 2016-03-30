31082934485471bf3ae50a167ad0642799f5612a
Mitch Kroska & Chris Bunkers <pair+kromitj.yaboichrissyb@devbootcamp.com>
2016-03-30 13:41:59 -0500

    User login, registration, logout, auth, navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,17 @@ get '/session/new' do
 end
 
 post '/session' do
-  @user = User.find_by(params[:user])
+  @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = ["Email/Password combination does not exist"]
+    erb :'/session/new'
+  end
+end
+
+delete '/session' do
+  session[:user_id] = nil
+  redirect '/questions'
 end
